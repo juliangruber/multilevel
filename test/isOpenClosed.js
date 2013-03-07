9549ef8a01c4805a57f491242e7320caa8f20958
Julian Gruber <julian@juliangruber.com>
2013-03-07 13:04:12 +0100

    some fixes for rpc-stream

--- test/isOpenClosed.js
+++ test/isOpenClosed.js
@@ -2,13 +2,11 @@ var getDb = require('./util').getDb
 var test = require('tap').test
 
 test('sync isOpen / isClosed', function (t) {
-  t.plan(3)
+  t.plan(2)
   
   getDb(function (db, dispose) {
     t.assert(db.isOpen())
     t.assert(!db.isClosed())
-    db.rpc.emit('end')
-    t.assert(db.isClosed())
     dispose()
   })
 })
