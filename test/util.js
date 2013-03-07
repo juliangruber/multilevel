c7200db6fa1b3d2a2a579d273b4d1ec95c8f4b28
Julian Gruber <julian@juliangruber.com>
2013-03-07 13:18:34 +0100

    use random port for testing

--- test/util.js
+++ test/util.js
@@ -3,12 +3,12 @@ var multilevel = require('..')
 var rimraf = require('rimraf')
 var net = require('net')
 var levelup = require('levelup')
+var freeport = require('freeport')
 
 util.getDb = function (cb) {
   rimraf(__dirname + '/db', function (err) {
     if (err) throw err
     
-    var port = 10000 + Math.round(Math.random() * 10000)
     var db = levelup(__dirname + '/db')
 
     var server = net.createServer(function (con) {
@@ -23,22 +23,27 @@ util.getDb = function (cb) {
 
       con.pipe(server).pipe(con)
     })
-    server.listen(port)
-  
-    var _db = multilevel.client()
-    var con = net.connect(port)
-    _db.pipe(con).pipe(_db)
 
-    _db.on('data', function (data) {
-      //console.log('C -> ' + data)
+    freeport(function (err, port) {
+      if (err) throw err
+
+      server.listen(port, function () {
+        var _db = multilevel.client()
+        var con = net.connect(port)
+        _db.pipe(con).pipe(_db)
+
+        _db.on('data', function (data) {
+          //console.log('C -> ' + data)
+        })
+
+        cb(_db, dispose)
+
+        function dispose () {
+          server.close()
+          db.close()
+          con.destroy()
+        }
+      })
     })
-
-    cb(_db, dispose)
-
-    function dispose () {
-      server.close()
-      db.close()
-      con.destroy()
-    }
   })
 }
