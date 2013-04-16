40dff89d2f3e999a2cb2ff9e65dead97bfa3c57b
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-16 15:15:29 +0100

    test for remote batch operations

---
+++ test/nested-batch.js
@@ -0,0 +1,33 @@
+var getDb = require('./util').getDb
+var test = require('tap').test
+var sublevel = require('level-sublevel')
+
+test('async', function (t) {
+  t.plan(2)
+
+  getDb(function (db) {
+    sublevel(db)
+    db.sublevel('foo')
+    db.sublevel('bar')
+  }, 
+  function (db, dispose) {
+    db.sublevel('foo').batch([
+      {key:'f', value:'1', type: 'put'},
+      {key:'b', value:'2', type: 'put', prefix: db.sublevel('bar').prefix()}
+    ],
+    function (err) {
+      if (err) throw err
+      db.sublevel('foo').get('f', function (err, value) {
+        if (err) throw err
+        t.equal(value, '1')
+        console.log('done1', value)
+        db.sublevel('bar').get('b', function (err, value) {
+          if (err) throw err
+          t.equal(value, '2')
+          console.log('done2', value)
+          dispose()
+        })
+      })
+    })
+  })
+})
