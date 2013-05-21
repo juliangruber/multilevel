18e763b3020a94682e4417d0c711fcd2ab2a7bab
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-21 14:06:45 +0200

    use dependency injection pattern to support buffers with binary or text

--- msgpack.js
+++ msgpack.js
@@ -1,4 +1,4 @@
-var MuxDemux = require('mux-demux/jsonb')
+var MuxDemux = require('mux-demux/msgpack')
 module.exports = {
   client : require('./lib/client')(MuxDemux),
   server : require('./lib/server')(MuxDemux)
