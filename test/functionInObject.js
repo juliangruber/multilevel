3557b541377a8ab9a8cda08b7b054199a103b622
Julian Gruber <julian@juliangruber.com>
2013-07-15 17:36:53 +0200

    added failing test for functions in objects

---
+++ test/functionInObject.js
@@ -0,0 +1,23 @@
+var getDb = require('./util').getDb;
+var test = require('tap').test;
+
+var obj = {
+  getImage: function () {},
+  a: 2
+};
+
+test('function in json', function (t) {
+  t.plan(3);
+  
+  getDb(function (db, dispose) {
+    debugger;
+    db.put('foo', obj, { valueEncoding: 'json' }, function (err) {
+      t.error(err);
+      db.get('foo', { valueEncoding: 'json' }, function (err, val) {
+        t.error(err);
+        t.equal(val.a, 2);
+        dispose();
+      });
+    });
+  });
+});
