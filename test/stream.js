ddd5a153e69b5077880a6c67cc454fdf37bea0e2
Julian Gruber <julian@juliangruber.com>
2013-07-16 09:02:26 +0200

    code style

--- test/stream.js
+++ test/stream.js
@@ -1,29 +1,29 @@
-var getDb = require('./util').getDb
-var test = require('tape')
+var getDb = require('./util').getDb;
+var test = require('tape');
 
 test('stream', function (t) {
-  t.plan(4)
+  t.plan(4);
   
   getDb(function (db, dispose) {
     db.put('foo', 'bar', function (err) {
-      if (err) throw err
+      if (err) throw err;
       
       db.readStream()
       .on('data', function (data) {
-        t.equal(data.key, 'foo')
-        t.equal(data.value, 'bar')
+        t.equal(data.key, 'foo');
+        t.equal(data.value, 'bar');
       })
       .on('end', function () {
-        var stream = db.writeStream()
-        stream.write({ key : 'bar', value : 'baz' })
+        var stream = db.writeStream();
+        stream.write({ key : 'bar', value : 'baz' });
         stream.on('close', function () {
           db.get('bar', function (err, value) {
-            t.notOk(err)
-            t.equal(value, 'baz')
-            dispose()
-          })
-        })
-        stream.end()
+            t.notOk(err);
+            t.equal(value, 'baz');
+            dispose();
+          });
+        });
+        stream.end();
       })
     })
   })
