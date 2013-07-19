d92c0d100cb16c3ceb98e619379545929b372ff1
Julian Gruber <julian@juliangruber.com>
2013-07-19 09:05:11 +0200

    code style

--- test/isOpenClosed.js
+++ test/isOpenClosed.js
@@ -1,12 +1,12 @@
-var getDb = require('./util').getDb
-var test = require('tape')
+var getDb = require('./util').getDb;
+var test = require('tape');
 
 test('sync isOpen / isClosed', function (t) {
-  t.plan(2)
+  t.plan(2);
   
   getDb(function (db, dispose) {
-    t.assert(db.isOpen())
-    t.assert(!db.isClosed())
-    dispose()
-  })
-})
+    t.assert(db.isOpen());
+    t.assert(!db.isClosed());
+    dispose();
+  });
+});
