a431db02bc5214a556af8cb3bd4b04d0774397a7
Julian Gruber <julian@juliangruber.com>
2013-08-07 23:34:28 -0400

    initial work

--- test/nested-async.js
+++ test/nested-async.js
@@ -1,24 +1,21 @@
 
-var sublevel = require('level-sublevel')
+var sublevel = require('level-sublevel');
 
 require('./util')(function (test, _, getDb) {
   test('async', function (t) {
-    t.plan(3)
+    t.plan(3);
   
     getDb(function (db) {
-      sublevel(db)
-      db.sublevel('foo')
-      t.notOk(db.isClient)
-
+      sublevel(db);
     }, 
     function (db, dispose) {
-      t.ok(db.isClient)
-      db.sublevels['foo'].put('foo', 'bar', function (err) {
-        if (err) throw err
-        db.sublevels['foo'].get('foo', function (err, value) {
-          if (err) throw err
-          t.equal(value, 'bar')
-          dispose()
+      t.ok(db.isClient);
+      db.sublevel('foo').put('foo', 'bar', function (err) {
+        if (err) throw err;
+        db.sublevel('foo').get('foo', function (err, value) {
+          if (err) throw err;
+          t.equal(value, 'bar');
+          dispose();
         })
       })
     })
