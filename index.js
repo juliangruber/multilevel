f1d26499fa55309cb576f336dbbc49a3f362d18a
Julian Gruber <julian@juliangruber.com>
2013-08-13 01:31:49 -0400

    expose writeManifest

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var MuxDemux = require('mux-demux/jsonb');
 
 module.exports = {
-  client : require('./lib/client')(MuxDemux),
-  server : require('./lib/server')(MuxDemux)
+  client: require('./lib/client')(MuxDemux),
+  server: require('./lib/server')(MuxDemux),
+  writeManifest: require('./lib/write-manifest')
 };
