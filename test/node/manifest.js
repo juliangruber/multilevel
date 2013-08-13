84b40284b00016804731700cc86a81291b264da1
Julian Gruber <julian@juliangruber.com>
2013-08-13 01:30:43 -0400

    added failing test for writeManifest

---
+++ test/node/manifest.js
@@ -0,0 +1,11 @@
+var test = require('tape');
+var writeManifest = require('../..').writeManifest;
+var level = require('level-test')();
+
+test('write manifest', function (t) {
+  var db = level();
+  writeManifest(db, __dirname + '/manifest.json');
+  var manifest = require('./manifest.json');
+  t.ok(manifest);
+  t.end();
+});
