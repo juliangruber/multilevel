fab9de83961e31a34ca6fe886198019417f8c774
Julian Gruber <julian@juliangruber.com>
2013-08-15 13:17:16 -0400

    added test for async db.close

--- test/isOpenClosed.js
+++ test/isOpenClosed.js
@@ -1,15 +1,36 @@
 require('./util')(function (test, _, getDb) {
 
-  test('sync isOpen / isClosed', function (t) {
+  test('sync isOpen / isClosed sync', function (t) {
     t.plan(4);
 
     getDb(function (db, dispose) {
       t.ok(db.isOpen());
       t.notOk(db.isClosed());
+
       db.close();
+
       t.notOk(db.isOpen());
       t.ok(db.isClosed());
+      
       dispose();
+    });
+  });
+
+  test('sync isOpen / isClosed async', function (t) {
+    t.plan(5);
+
+    getDb(function (db, dispose) {
+      t.ok(db.isOpen());
+      t.notOk(db.isClosed());
+
+      db.close(function(err) {
+        t.error(err);
+
+        t.notOk(db.isOpen());
+        t.ok(db.isClosed());
+
+        dispose();
+      });
     });
   });
 
