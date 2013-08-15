d675e77d3fdff5b0a54d60feb2d0a1c78a58ccf0
Julian Gruber <julian@juliangruber.com>
2013-08-15 13:57:16 -0400

    80 chars

--- test/binary.js
+++ test/binary.js
@@ -5,10 +5,11 @@ var binary = require('bops');
 require('./util')(function (test, _, getDb) {
 
   test('binary', function (t) {
+    var opts = { valueEncoding : 'binary' };
     getDb(function (db, dispose) {
-      db.put('foo', binary.from([1,2,3]), { valueEncoding : 'binary' }, function (err) {
+      db.put('foo', binary.from([1,2,3]), opts, function (err) {
         t.error(err);
-        db.get('foo', { valueEncoding : 'binary' }, function (err, value) {
+        db.get('foo', opts, function (err, value) {
           t.error(err);
           t.ok(binary.is(value));
           t.equal(binary.to(value), binary.to(binary.from([1,2,3])));
