5244f31c99a27afc78f43fbc8bdfddccc024fbbd
Julian Gruber <julian@juliangruber.com>
2013-11-26 17:15:48 +0100

    fix deep nested object issue

--- test/nested.js
+++ test/nested.js
@@ -1,28 +1,44 @@
 require('./util')(function (test, _, getDb) {
 
   test('nested', function (t) {
-    t.plan(4);
+    t.plan(7);
 
     getDb(function (db) {
       db.nested = {
-        get: function(cb) { cb(null, 'hola') }
+        get: function(cb) { cb(null, 'hola') },
+        deeper: {
+          get: function(cb) { cb(null, 'ola') }
+        }
       };
-      db.methods = db.methods || {};
-      db.methods.nested = {
-        type: 'object',
-        methods: {
-          get: { type: 'async' }
+      db.methods = {
+        nested: {
+          type: 'object',
+          methods: {
+            get: { type: 'async' },
+            deeper: {
+              type: 'object',
+              methods: {
+                get: { type: 'async' }
+              }
+            }
+          }
         }
       };
       return { db: db };
     },
     function (db, dispose) {
-      t.ok(db.nested);
-      t.ok(db.nested.get);
+      t.ok(db.nested, 'has nested');
+      t.ok(db.nested.get, 'has nested.get');
+      t.ok(db.nested.deeper.get, 'has nested.deeper.get');
       
       db.nested.get(function(err, value) {
-        t.error(err);
-        t.equal(value, 'hola');
+        t.error(err, 'nested.get without error');
+        t.equal(value, 'hola', 'nested.get has value');
+        
+        db.nested.deeper.get(function(err, value) {
+          t.error(err, 'nested.deeper.get without error');
+          t.equal(value, 'ola', 'nested.deeper.get has value');
+        });
       });
     });
   });
